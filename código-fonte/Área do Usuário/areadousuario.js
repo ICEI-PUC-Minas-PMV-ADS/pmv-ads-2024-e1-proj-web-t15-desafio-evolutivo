3ad94bcfa7e19b7eb81a17e7035d86f82ddfdb11
sharonsteicys <166241111+sharonsteicys@users.noreply.github.com>
2024-05-25 21:16:48 -0300

    Update areadousuario.js

--- código-fonte/Área do Usuário/areadousuario.js
+++ código-fonte/Área do Usuário/areadousuario.js
@@ -1,42 +1,64 @@
-// Função para importar os dados senha e email do local storage, se disponíveis
-function importarValor() {
-  const email = localStorage.getItem('cadastro-email');
-  const senha = localStorage.getItem('cadastro-password');
+var dados = JSON.parse(localStorage.getItem("chave"));
 
-  if (email && senha) {
-    document.getElementById('conteudo-principal-dados-email').value = email;
-    document.getElementById('conteudo-principal-dados-senha').value = senha;
-  }
+document.getElementById("dadosnome").value = dados.email;
+
+function carregarDados() {
+  // Exibindo dados em um campo de input
+  document.getElementById("dadosnome").value = dados.nome;
+  document.getElementById("dadosemail").value = dados.email;
+  document.getElementById("dadospassword").value = dados.senha;
 }
 
 // Função para habilitar edição dos campos
-document.getElementById('editar').addEventListener('click', function() {
+document.getElementById("editar").addEventListener("click", function () {
   // Habilitar todos os campos de entrada
-  const inputs = document.querySelectorAll('#dados-form input');
-  inputs.forEach(input => input.disabled = false);
+  const inputs = document.querySelectorAll("#dados-form input");
+  inputs.forEach((input) => (input.disabled = false));
 
   // Esconder o botão de editar e mostrar o botão de salvar
-  document.getElementById('editar').style.display = 'none';
-  document.getElementById('salvar').style.display = 'inline';
+  document.getElementById("editar").style.display = "none";
+  document.getElementById("salvar").style.display = "inline";
+
+  document.getElementById("salvar").addEventListener("click", function () {
+    // Quando o botão 'salvar' for clicado, esconda-o e mostre o botão 'editar'
+    const inputs = document.querySelectorAll("#dados-form input");
+    inputs.forEach((input) => (input.disabled = true));
+    document.getElementById("salvar").style.display = "none";
+    document.getElementById("editar").style.display = "inline";
+  });
 });
+
+const salve = document.querySelector("#salvar") 
+
+if (salve) {
+    salve.addEventListener("click", () => salvar())
+}
+function salvar() {
+    dados.nome = document.getElementById("dadosnome").value;
+    dados.email = document.getElementById("dadosemail").value;
+    dados.senha = document.getElementById("dadospassword").value;
+    console.log(dados)
+
+    localStorage.setItem("chave", JSON.stringify(dados));
+}
 
 // Função para mostrar a foto de perfil
 function mostrarFotodePerfil() {
-  const input = document.getElementById('foto-input');
-  const output = document.getElementById('conteudo-principal-perfil-anon');
+  const input = document.getElementById("foto-input");
+  const output = document.getElementById("conteudo-principal-perfil-anon");
 
   input.onchange = function (e) {
     const file = e.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = function(event) {
+    reader.onload = function (event) {
       output.src = event.target.result;
-      output.style.width = '250px';
-      output.style.height = '250px';
-      localStorage.setItem('profilepic', event.target.result);
-      const avatar = document.querySelector('.cabecalho-avatar');
-      avatar.src = localStorage.getItem('profilepic');
-    }
+      output.style.width = "250px";
+      output.style.height = "250px";
+      localStorage.setItem("profilepic", event.target.result);
+      const avatar = document.querySelector(".cabecalho-avatar");
+      avatar.src = localStorage.getItem("profilepic");
+    };
 
     reader.readAsDataURL(file);
   };
@@ -46,19 +68,19 @@ function mostrarFotodePerfil() {
 
 // Função para carregar a imagem de perfil do local storage
 function carregarImagemDePerfil() {
-  const fotosalva = localStorage.getItem('profilepic');
+  const fotosalva = localStorage.getItem("profilepic");
 
   if (fotosalva) {
-    const avatar = document.querySelector('.cabecalho-avatar');
+    const avatar = document.querySelector(".cabecalho-avatar");
     avatar.src = fotosalva;
-    const perfil = document.querySelector('#conteudo-principal-perfil-anon');
+    const perfil = document.querySelector("#conteudo-principal-perfil-anon");
     perfil.src = fotosalva;
-    document.getElementById('botao-imagem').innerText = 'Trocar imagem';
+    document.getElementById("botao-imagem").innerText = "Trocar imagem";
   }
 }
 
 // Carregar dados do local storage e imagem de perfil quando a página é carregada
-window.onload = function() {
-  importarValor();
+window.onload = function () {
   carregarImagemDePerfil();
+  carregarDados();
 };
