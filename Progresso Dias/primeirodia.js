54824973a0d94bc51d429e1104ff71ed708600c0
Gustavo Paz <166300104+Gustavogpaz@users.noreply.github.com>
2024-06-05 10:09:36 -0300

    Add files via upload

--- Progresso Dias/primeirodia.js
+++ Progresso Dias/primeirodia.js
@@ -1,37 +1,92 @@
 
-var dados = JSON.parse(localStorage.getItem("chave"));
+$(document).ready(function() {
+  $('#my-button').click(function() {
+    $('#popup-modal').fadeIn();
+  });
+  
+  $('#ok-button').click(function() {
+    window.location.href = "segundodia.html";
+  });
+  
+  $('cancel-button').click(function() {
+    window.location.href = "segundodia.html";
+  });
 
-document.getElementById("dadosnome").value = dados.email;
-
-function carregarDados() {
-  // Exibindo dados em um campo de input
-  document.getElementById("dadosnome").value = dados.nome;
-  document.getElementById("dadosemail").value = dados.email;
-  document.getElementById("dadospassword").value = dados.senha;
-  document.getElementById("dadosprogresso").value = dados.prog;
-  document.getElementById("dadosdata").value = dados.data;
-}
-
-// Função para habilitar edição dos campos
-document.getElementById("mybutton").addEventListener("click", function () {
-  dados.prog = getElementById.prog.value;
-  if(dados.prog < 1 || dados.prog !=0, 0.333 + dados.prog);
-  else("");
 });
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Recupera o contador e a data do último desafio do localStorage
+  let contador = parseInt(localStorage.getItem('contador')) || 0; //alterar esse número para forçar teste
+  let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+  
+  // Exibe o contador atual na página
+  // document.getElementById('contador').textContent = `${contador}/30`;
+  // Atualiza o contador se o botão for clicado
+  document.getElementById('cancel-button').addEventListener('click', () => {
+      const today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD               
+      
+      // Verifica se o desafio já foi feito hoje
+      if (today !== lastChallengeDate && contador < 30) {
+          contador = Math.min(contador + 1); // Incrementa o contador
+          lastChallengeDate = today;
+          // Atualiza o contador e a data do último desafio no localStorage
+          localStorage.setItem('contador', contador);
+          localStorage.setItem('lastChallengeDate', lastChallengeDate);
+          // Atualiza o contador na página
+          // document.getElementById('contador').textContent = `${contador}/30`;
+          
+          // Vitória no desafio
+          if (contador >= 30) {
+              alert('Parabéns! Você venceu o desafio!');
+          } 
+      }
+      else {
+          alert('Você já completou o desafio de hoje!');
+      }
+      //A pessoa pulou um dia e perdeu o desafio
+      //if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 1) {
+      //    alert('Que vacilo! Você falhou no desafio. Mas não se preocupe, você terá uma chance!');
+      //    contador = Math.min(contador + 1);
+      //   chancedevacilo = chancedevacilo - 1;
+      //    lastChallengeDate = today;
+      //}
+      //else if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 0) {
+      //   alert('Volte ao início do desafio! Tente novamente, você irá conseguir!');
+      //    contador = 0;
+      //}
+  });
+});
+// var dados = JSON.parse(localStorage.getItem("chave"));
 
-const salve = document.querySelector("#salvar") 
+// document.getElementById("dadosnome").value = dados.email;
 
-if (salve) {
-    salve.addEventListener("click", () => salvar())
-}
-function salvar() {
-    dados.nome = document.getElementById("dadosnome").value;
-    dados.email = document.getElementById("dadosemail").value;
-    dados.senha = document.getElementById("dadospassword").value;
-    console.log(dados)
-    document.getElementById("dadosprogresso").value;
-    document.getElementById("dadosdata").value;
+// function carregarDados() {
+//   // Exibindo dados em um campo de input
+//   document.getElementById("dadosnome").value = dados.nome;
+//   document.getElementById("dadosemail").value = dados.email;
+//   document.getElementById("dadospassword").value = dados.senha;
+//   document.getElementById("dadosprogresso").value = dados.prog;
+//   document.getElementById("dadosdata").value = dados.data;
+// }
 
-    localStorage.setItem("chave", JSON.stringify(dados));
-}
+// // Função para habilitar edição dos campos
+// document.getElementById("#cancel-button").addEventListener("click", function () {
+//   dados.prog = getElementById.prog.value;
+//   if(dados.prog < 1 || dados.prog !=0, 0.333 + dados.prog);
+//   else("");
+// });
 
+// const salve = document.querySelector("#salvar") 
+
+// if (salve) {
+//     salve.addEventListener("click", () => contar())
+// }
+// function contar() {
+//     dados.nome = document.getElementById("dadosnome").value;
+//     dados.email = document.getElementById("dadosemail").value;
+//     dados.senha = document.getElementById("dadospassword").value;
+//     console.log(dados)
+//     document.getElementById("dadosprogresso").value;
+//     document.getElementById("dadosdata").value;
+
+//     localStorage.setItem("chave", JSON.stringify(dados));
+// }
--- Progresso Dias/trigesimodia.js
+++ Progresso Dias/trigesimodia.js
@@ -1,55 +1,56 @@
 $(document).ready(function() {
-    $('#my-button').click(function() {
-      $('#popup-modal').fadeIn();
-    });
-    
-    $('#ok-button').click(function() {
-      window.location.href = "https://www.example.com/new-page";
-    });
-    
-    $('#cancel-button').click(function() {
-      window.location.href = "Desafio dia 1/Desafio dia 1.HTML";
-    });
+  $('#my-button').click(function() {
+    $('#popup-modal').fadeIn();
+  });
   
+  $('#ok-button').click(function() {
+    window.location.href = "segundodia.html";
+  });
+  
+  $('cancel-button').click(function() {
+    window.location.href = "segundodia.html";
   });
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-      // Recupera o contador e a data do último desafio do localStorage
-      let contador = parseInt(localStorage.getItem('contador')) || 29; //alterar esse número para forçar teste
-      let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Recupera o contador e a data do último desafio do localStorage
+  let contador = parseInt(localStorage.getItem('contador')) || 29; //alterar esse número para forçar teste
+  let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+  
+  // Exibe o contador atual na página
+  // document.getElementById('contador').textContent = `${contador}/30`;
+  // Atualiza o contador se o botão for clicado
+  document.getElementById('cancel-button').addEventListener('click', () => {
+      const today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD               
       
-      // Exibe o contador atual na página
-      document.getElementById('contador').textContent = `Desafios completados: ${contador}`;
-      // Atualiza o contador se o botão for clicado
-      document.getElementById('cancel-button').addEventListener('click', () => {
-          const today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD               
+      // Verifica se o desafio já foi feito hoje
+      if (today !== lastChallengeDate && contador < 30) {
+          contador = Math.min(contador + 1); // Incrementa o contador
+          lastChallengeDate = today;
+          // Atualiza o contador e a data do último desafio no localStorage
+          localStorage.setItem('contador', contador);
+          localStorage.setItem('lastChallengeDate', lastChallengeDate);
+          // Atualiza o contador na página
+          // document.getElementById('contador').textContent = `${contador}/30`;
           
-          // Verifica se o desafio já foi feito hoje
-          if (today !== lastChallengeDate && contador < 30) {
-          
-              lastChallengeDate = today;
-              contador ++;// Incrementa o contador
-              // Atualiza o contador e a data do último desafio no localStorage
-              localStorage.setItem('contador', contador);
-              localStorage.setItem('lastChallengeDate', lastChallengeDate);
-              // Vitória no desafio
-              if (contador >= 30) {
-                  alert('Parabéns! Você venceu o desafio!');
-              } 
-          }
-          else {
-              alert('Você já completou o desafio de hoje!');
-          }
-          //A pessoa pulou um dia e perdeu o desafio
-          //if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 1) {
-          //    alert('Que vacilo! Você falhou no desafio. Mas não se preocupe, você terá uma chance!');
-          //    contador = Math.min(contador + 1);
-          //   chancedevacilo = chancedevacilo - 1;
-          //    lastChallengeDate = today;
-          //}
-          //else if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 0) {
-          //   alert('Volte ao início do desafio! Tente novamente, você irá conseguir!');
-          //    contador = 0;
-          //}
-      });
+          // Vitória no desafio
+          if (contador >= 30) {
+              alert('Parabéns! Você venceu o desafio!');
+          } 
+      }
+      else {
+          alert('Você já completou o desafio de hoje!');
+      }
+      //A pessoa pulou um dia e perdeu o desafio
+      //if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 1) {
+      //    alert('Que vacilo! Você falhou no desafio. Mas não se preocupe, você terá uma chance!');
+      //    contador = Math.min(contador + 1);
+      //   chancedevacilo = chancedevacilo - 1;
+      //    lastChallengeDate = today;
+      //}
+      //else if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 0) {
+      //   alert('Volte ao início do desafio! Tente novamente, você irá conseguir!');
+      //    contador = 0;
+      //}
   });
+});
