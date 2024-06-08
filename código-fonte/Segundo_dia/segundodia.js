09b0713f518105ff5d5950bd7649842665929ba3
Artur M <163445754+artma1@users.noreply.github.com>
2024-06-08 16:39:13 -0300

    update segundodia
    FINAL RESOLVIDO!!!!!!!!

--- código-fonte/Segundo_dia/segundodia.js
+++ código-fonte/Segundo_dia/segundodia.js
@@ -1,38 +1,45 @@
+//declarando as variáveis para usar em tudo
+var contador = parseInt(localStorage.getItem('contador')) || 0; //alterar esse número para forçar teste
+var lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+var today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD    
+  
 $(document).ready(function() {
   $('#my-button').click(function() {
+    if (lastChallengeDate !== today) {
     $('#popup-modal').fadeIn();0
     $(this).hide(); // Esconde o botão "Iniciar"
-  });
-  
+  } else {
+    alert('Você já fez seu desafio hoje! Volte amanhã!');
+  }});
+
   $('#ok-button').click(function() {
     window.location.href = "segundodia.html";
   });
   
   $('#cancel-button').click(function() {
-    alert("Parabéns pela conclusão da tarefa!");
     window.location.href = "trigesimodia.html";
   });
 });
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Recupera o contador e a data do último desafio do localStorage
-  let contador = parseInt(localStorage.getItem('contador')) || 0; //alterar esse número para forçar teste
-  let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
-  
-  // Exibe o contador atual na página
-  // document.getElementById('contador').textContent = `${contador}/30`;
+
   // Atualiza o contador se o botão for clicado
   document.getElementById('cancel-button').addEventListener('click', () => {
-      const today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD               
-      
       // Verifica se o desafio já foi feito hoje
+      let contador = parseInt(localStorage.getItem('contador')) || 0; //alterar esse número para forçar teste
+      let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+      let today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD
+
       if (today !== lastChallengeDate && contador < 30) {
           contador = Math.min(contador + 1); // Incrementa o contador
+
+          if  ( today !== lastChallengeDate){
+            alert("Parabéns pela conclusão da tarefa!");
+            window.location.href = "trigesimodia.html";}
           lastChallengeDate = today;
           // Atualiza o contador e a data do último desafio no localStorage
           localStorage.setItem('contador', contador);
           localStorage.setItem('lastChallengeDate', lastChallengeDate);
-          // Atualiza o contador na página
-          // document.getElementById('contador').textContent = `${contador}/30`;
           
           // Vitória no desafio
           if (contador >= 30) {
@@ -41,17 +48,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
       }
       else {
           alert('Você já completou o desafio de hoje!');
-      }
-      //A pessoa pulou um dia e perdeu o desafio
-      //if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 1) {
-      //    alert('Que vacilo! Você falhou no desafio. Mas não se preocupe, você terá uma chance!');
-      //    contador = Math.min(contador + 1);
-      //   chancedevacilo = chancedevacilo - 1;
-      //    lastChallengeDate = today;
-      //}
-      //else if (today !== lastChallengeDate && yesterday !== lastChallengeDate && chancedevacilo == 0) {
-      //   alert('Volte ao início do desafio! Tente novamente, você irá conseguir!');
-      //    contador = 0;
-      //}
+      }  
   });
 });
+// Função para carregar a imagem de perfil do local storage
+function carregarImagemDePerfil() {
+  const fotosalva = localStorage.getItem("profilepic");
+
+  if (fotosalva) {
+    const avatar = document.querySelector(".cabecalho-avatar");
+    avatar.src = fotosalva;
+     }
+}
+
+// Carregar dados do local storage e imagem de perfil quando a página é carregada
+window.onload = function () {
+  carregarImagemDePerfil();
+  
+};
