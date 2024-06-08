f0a219fd84534677076b7ca31ce196eae0edc221
Artur M <163445754+artma1@users.noreply.github.com>
2024-06-08 17:33:15 -0300

    Update homepage.js

--- codigo-fonte backup/homepage.js
+++ codigo-fonte backup/homepage.js
@@ -10,20 +10,44 @@ function myFunction(x) {
       document.getElementById("myNav").style.width = "0%";
   }
 
-  // Função para carregar a imagem de perfil do local storage
-function carregarImagemDePerfil() {
-    const fotosalva = localStorage.getItem("profilepic");
-  
-    if (fotosalva) {
-      const avatar = document.querySelector(".cabecalho-avatar");
-      avatar.src = fotosalva;
-       }
-  }
- // Selecionando o elemento
-// Selecionando o elemento
+  function alertaDesafio (){
+  let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+  let contador = localStorage.getItem("contador");
+  const hoje = new Date();
 
-  // Carregar dados do local storage e imagem de perfil quando a página é carregada
-  window.onload = function () {
-    carregarImagemDePerfil();
-    
+  if (lastChallengeDate && hoje !== lastChallengeDate && 0 < contador < 30){
+    alert("Você está inscrito em um ou mais desafios! \nNão se esqueça de fazer hoje!")
+  }}
+
+  function enviarDados() {
+    // Obter dados do localStorage
+    const dados = JSON.parse(localStorage.getItem('chave'));
+    let lastChallengeDate = localStorage.getItem('lastChallengeDate') || '';
+  
+    const today = new Date().toLocaleDateString('pt-br'); 
+  
+    if (dados && today !== lastChallengeDate) {
+        // Enviar dados para o servidor por meio de uma requisição AJAX
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:3000/receber-dados');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log('Dados enviados com sucesso!');
+            } else {
+                console.error('Erro ao enviar dados:', xhr.statusText);
+            }
+        };
+        xhr.onerror = function() {
+            console.error('Erro ao enviar dados:', xhr.statusText);
+        };
+        xhr.send(JSON.stringify(dados));
+    } else {
+        console.error('Dados não encontrados no localStorage.');
+    }
   };
+
+  window.onload = function () {
+    alertaDesafio();
+    enviarDados();
+    };
